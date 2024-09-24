bc8da0f09d9e165e0d2f8a377e8c2a9cad8ce312
AsHim Shrestha <ashimsth89@gmail.com>
2024-09-24 21:19:53 +0545

    Add Prisma client and user mutation resolver

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,53 @@
 import express from "express";
-
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prisma } from "./lib/script";
 
 async function startApolloServer() {
   const app = express();
-
   app.use(express.json());
 
   const PORT = process.env.PORT || 8000;
 
   const gqlServer = new ApolloServer({
     typeDefs: `
-        type Query {
-            hello: String    
-        }
-        `,
+      type User {
+        id: String!
+        name: String!
+        email: String!
+        password: String!
+        salt: String!
+        userImage: String
+        createdAt: String!
+        updatedAt: String!
+      }
+
+      type Query {
+        hello: String    
+      }
+
+      type Mutation {
+        createUser(name: String!, email: String!, password: String!): User!
+      }
+    `,
+
     resolvers: {
       Query: {
         hello: () => "Hello World",
+      },
+
+      Mutation: {
+        createUser: async (_, { name, email, password }: { name: string; email: string; password: string }) => {
+          const newUser = await prisma.user.create({
+            data: {
+              name,
+              email,
+              password,
+              salt: "heythere",
+            },
+          });
+          return newUser;
+        },
       },
     },
   });
